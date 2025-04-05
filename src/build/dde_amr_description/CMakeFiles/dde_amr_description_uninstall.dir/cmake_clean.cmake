309d4a0deeddc9e4f0aa5a038a93251e3b655dc2
tommysnu <tommy@datadesign.engineering>
2025-04-06 00:08:12 +0900

    Upload controller

--- src/build/dde_amr_description/CMakeFiles/dde_amr_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dde_amr_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dde_amr_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
